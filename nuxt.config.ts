b323278b1c7e7e20bad2594d15d83145df080e34
felipe.ramalho <felipe.ramalho@codeminer42.com>
2026-02-01 21:15:20 -0300

    ci: update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
+  modules: ["@nuxtjs/i18n"],
+  css: ["./app/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  i18n: {
+    locales: [
+      { code: "pt", name: "PT", file: "pt.json" },
+      { code: "en", name: "EN", file: "en.json" },
+    ],
+    defaultLocale: "pt",
+    lazy: true,
+    langDir: "locales",
+    strategy: "no_prefix",
+  },
 });
